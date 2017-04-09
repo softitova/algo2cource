dbc49c5a5dfab275cf8944a93d919085871834ee
sofatitovass <sof_titova@rambler.ru>
2017-04-09 19:15:45 +0300

    all tests passed

--- veb/VEBTree.java
+++ veb/VEBTree.java
@@ -1,5 +1,9 @@
 package veb;
 
+import veb.IntegerSet;
+
+import java.util.HashMap;
+
 /**
  * Created by Sophia Titova on 22.03.17.
  */
@@ -11,39 +15,29 @@ public class VEBTree implements IntegerSet {
         root = new VEBNode(k);
     }
 
-    @Override
+
     public void add(long x) {
         addT(root, x);
     }
 
-    @Override
     public void remove(long x) {
         removeT(root, x);
     }
 
-    @Override
     public long next(long x) {
         return nextT(root, x);
     }
 
-    @Override
     public long prev(long x) {
         return prevT(root, x);
     }
 
-    @Override
     public long getMin() {
         return root.min;
     }
 
-    @Override
     public long getMax() {
         return root.max;
-    }
-
-
-    public boolean find(long x) {
-        return findR(root, x);
     }
 
     private long high(VEBNode node, long key) {
@@ -51,27 +45,25 @@ public class VEBTree implements IntegerSet {
     }
 
     private long low(VEBNode node, long key) {
-        return (int) (key & ((1L << (node.k / 2)) - 1L));
+        return (key & ((1L << (node.k / 2)) - 1L));
     }
 
     private long merge(VEBNode node, long high, long low) {
-        return (high << (node.k / 2)) + low;
-    }
-
-    private boolean findR(VEBNode root, long x) {
-        return !empty(root)
-                && ((root.min == x || root.max == x)
-                || findR(root.clusters[(int) high(root, x)], (int) low(root, x)));
+        return ((high << (node.k / 2))) | low;
     }
 
     private boolean empty(VEBNode root) {
-        return root.min == NO;
+        return root != null && root.min == NO;
     }
+
 
     private void addT(VEBNode node, long x) {
         if (empty(node)) {
             node.min = x;
             node.max = x;
+            return;
+        }
+        if (x == node.min) {
             return;
         }
         if (x < node.min) {
@@ -83,84 +75,109 @@ public class VEBTree implements IntegerSet {
             node.max = x;
         }
         if (node.k != 1) {
-            if (empty(node.clusters[(int) high(node, x)])) {
+            node.clusters.putIfAbsent(high(node, x), new VEBNode(node.k / 2));
+            if (empty(node.clusters.get(high(node, x)))) {
                 addT(node.summary, high(node, x));
             }
-            addT(node.clusters[(int) high(node, x)], (int) low(node, x));
+            addT(node.clusters.get(high(node, x)), low(node, x));
         }
     }
 
     private void removeT(VEBNode node, long x) {
-
-        if(x == node.max) {
+        if (node.min == node.max) {
+            node.min = node.max = NO;
+            return;
+        }
+        if (x == node.max) {
+            if (node.k == 1) {
+                node.max = node.min;
+                return;
+            }
             node.max = prevT(node, node.max);
         }
-
         if (x == node.min) {
-
-            if (node.k == 1 || empty(node.summary)) { // память / сравнение на нулл
+            if (node.k == 1) {
+                node.min = node.max;
+                return;
+            }
+            if (empty(node.summary)) { // память / сравнение на нулл
                 node.max = node.min = NO;
                 return;
             }
 
-            int summaryMin = (int) node.summary.min;
-            VEBNode clusterSumMin = node.clusters[summaryMin];
+            VEBNode clusterSumMin = node.clusters.get(node.summary.min);
+            node.min = merge(node, node.summary.min, clusterSumMin.min);
 
-            node.min = merge(node, summaryMin, clusterSumMin.min);
             removeT(clusterSumMin, clusterSumMin.min);
 
             if (clusterSumMin.min == NO) {
-                removeT(node.summary, summaryMin);
+                removeT(node.summary, node.summary.min);
             }
             return;
         }
 
-// максимум обнулить
-        int curH = (int) high(node, x);
-        int curL = (int) low(node, x);
+        long curH = high(node, x);
+        long curL = low(node, x);
 
-        removeT(node.clusters[curH], curL);
+        if (!node.clusters.containsKey(curH)) {
+            return;
+        }
+        removeT(node.clusters.get(curH), curL);
 
-        if (node.clusters[curH].min == NO) {
+        if (node.clusters.get(curH).min == NO) {
             removeT(node.summary, curH);
         }
+        if (x == node.max) {
+
+            curH = high(node, x);
+            VEBNode curN = node.clusters.get(curH);
+
+            if (curN == null) {
+                if (empty(node.summary)) {
+                    node.min = node.max = NO;
+                    return;
+                }
+                curH = node.summary.max;
+            }
+            node.max = merge(node, curH, node.clusters.get(curH).max);
+        }
     }
-//    add 1
-//    add 17
-//    add 389
-//    add 5
-//    add 144
-//    prev 144
-//            17
-//    remove 389
-//    max
-//    389
-//    dump
+
     private long nextT(VEBNode node, long x) {
 
+        if (node.k == 1) {
+            if (node.min == NO || x >= node.max) {
+                return NO;
+            }
+            return x < node.min ? node.min : node.max;
+        }
         if (empty(node) || node.max <= x) {
             return NO;
         }
         if (node.min > x) {
             return node.min;
         }
-        if (empty(node.summary)) {
-            return node.max;
-        }
 
-        int curH = (int) high(node, x);
-        int curL = (int) low(node, x);
-        VEBNode clusterCurH = node.clusters[curH];
+        long curH = high(node, x); // empty(node.summary);
+        long curL = low(node, x);
+        VEBNode clusterCurH = node.clusters.get(curH);
 
-        if (!empty(clusterCurH) && clusterCurH.max > curL) {
+        if (clusterCurH != null && !empty(clusterCurH) && clusterCurH.max > curL) {
             return merge(node, curH, nextT(clusterCurH, curL));
         }
 
         long n = nextT(node.summary, curH);
-        return n == NO ? node.max : merge(node, n, node.clusters[(int) n].min);
+        return n == NO ? node.max : merge(node, n, node.clusters.get(n).min);
     }
 
     private long prevT(VEBNode node, long x) {
+
+        if (node.k == 1) {
+            if (node.min == NO || x <= node.min) {
+                return NO;
+            }
+            return x > node.max ? node.max : node.min;
+        }
 
         if (empty(node) || node.min >= x) {
             return NO;
@@ -169,14 +186,40 @@ public class VEBTree implements IntegerSet {
             return node.max;
         }
 
-        int curH = (int) high(node, x);
-        int curL = (int) low(node, x);
-        VEBNode clusterCurH = node.clusters[curH];
+        long curH = high(node, x);
+        long curL = low(node, x);
+        VEBNode clusterCurH = node.clusters.get(curH);
 
-        if (!empty(clusterCurH) && clusterCurH.min < curL) {
+        if (clusterCurH != null && !empty(clusterCurH) && clusterCurH.min < curL) {
             return merge(node, high(node, x), prevT(clusterCurH, curL));
         }
         long n = prevT(node.summary, high(node, x));
-        return n == NO ? node.min : merge(node, n, node.clusters[(int) n].max);
+        return n == NO ? node.min : merge(node, n, node.clusters.get(n).max);
+    }
+
+
+    private class VEBNode {
+        int k;
+        VEBNode summary;
+        HashMap<Long, VEBNode> clusters;
+        long max = NO;
+        long min = NO;
+
+        VEBNode(int k) {
+            this.k = k;
+            initializeChildren(k);
+        }
+
+        private void initializeChildren(int k) {
+            if (k == 1) {
+                summary = null;
+                clusters = null;
+            } else {
+
+                int childSize = k / 2;
+                summary = new VEBNode(childSize);
+                clusters = new HashMap<>();
+            }
+        }
     }
 }
